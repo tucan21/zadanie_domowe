ff34b4d64af390bd24bb12a11849dad51af6902e
tucan21 <stat21@interia.pl>
2017-04-22 15:32:12 +0200

    new contacts

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, address, mobile, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.address = address
+        self.mobile = mobile
+        self.email = email
